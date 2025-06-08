04aa24f163431fdd95a840560f0ecd65c520d98e
alan-bezerra <alanbezerra_@outlook.com>
2025-06-08 11:03:05 -0300

    Projeto completo adicionado + Front(HTML, CSS e JS)

--- prontoConsultaMonoConfigurado/src/main/java/com/prontoconsulta/repository/MedicoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.prontoconsulta.repository;
-
-import com.prontoconsulta.entity.Medico;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MedicoRepository extends JpaRepository<Medico, Long> {}
--- prontoConsultaMonoConfigurado/src/main/java/com/prontoconsulta/repository/PacienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.prontoconsulta.repository;
-
-import com.prontoconsulta.entity.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PacienteRepository extends JpaRepository<Paciente, Long> {}
